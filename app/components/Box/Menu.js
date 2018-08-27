e1e808570812b3fa8552e4eec40eee009c474740
Rudy Ondolan <rudy.ondolan@myrepublic.net>
2018-08-27 09:18:45 +0700

    refactor

--- app/components/Box/Menu.js
+++ app/components/Box/Menu.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import A from 'components/A';
+
 const MenuBoxWrapper = styled.div`
   position: relative;
   padding: 0 20px;
@@ -21,12 +23,45 @@ const MenuBoxWrapper = styled.div`
     background: #fff;
     border-bottom: 1px solid #e5e5e5;
   }
+
+  li {
+    width: auto;
+    flex: 1;
+    -webkit-box-flex: 1;
+    display: flex;
+    text-align: center;
+    background: #fff;
+    -webkit-box-pack: center;
+    justify-content: center;
+    -webkit-box-align: center;
+  }
+
+  a {
+    margin-left: 0;
+    color: #ff7d1d;
+    border-bottom: 2px solid #ff7d1d;
+    padding: 0;
+    margin: 0 8px;
+    font-weight: 400;
+    line-height: 60px;
+    text-decoration: none;
+    width: 100%;
+    text-overflow: unset;
+    letter-spacing: 1px;
+    display: block;
+    height: 48px;
+    overflow: hidden;
+    white-space: nowrap;
+  }
 `;
 
 function MenuBox(props) {
   return (
     <MenuBoxWrapper>
       <ul>
+        <li>
+          <A to="">Pulsa</A>
+        </li>
       </ul>
     </MenuBoxWrapper>
   );
